2ea7e377686c3bb8ee32c73ee6aeebfc1cde1922
Claudio Soares <claudiolibanez@gmail.com>
2023-02-05 00:35:57 -0300

    Exibe personagens da API da Marvel

--- app/src/main/java/com/example/marvelapp/presentation/characters/CharactersFragment.kt
+++ app/src/main/java/com/example/marvelapp/presentation/characters/CharactersFragment.kt
@@ -5,16 +5,22 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.lifecycleScope
 import com.example.core.domain.model.Character
 import com.example.marvelapp.R
 import com.example.marvelapp.databinding.FragmentCharactersBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class CharactersFragment : Fragment() {
 
     private var _binding: FragmentCharactersBinding? = null
     private val binding: FragmentCharactersBinding get() = _binding!!
+
+    private val viewModel: CharactersViewModel by viewModels()
 
     private val charactersAdapter = CharactersAdapter()
 
@@ -35,15 +41,38 @@ class CharactersFragment : Fragment() {
 
         initCharactersAdapter()
 
-        charactersAdapter.submitList(
-            listOf(
-                Character("Spider Man", "https://lumiere-a.akamaihd.net/v1/images/marvelspidermanseries-emeagrid-m_4456fb99.jpeg"),
-                Character("Spider Man", "https://lumiere-a.akamaihd.net/v1/images/marvelspidermanseries-emeagrid-m_4456fb99.jpeg"),
-                Character("Spider Man", "https://lumiere-a.akamaihd.net/v1/images/marvelspidermanseries-emeagrid-m_4456fb99.jpeg"),
-                Character("Spider Man", "https://lumiere-a.akamaihd.net/v1/images/marvelspidermanseries-emeagrid-m_4456fb99.jpeg")
-            )
-        )
+        lifecycleScope.launch {
+            viewModel.charactersPagingData("").collect { pagingData ->
+                charactersAdapter.submitData(pagingData)
+            }
+        }
+
+//        charactersAdapter.submitList(
+//            fakeList()
+//        )
     }
+
+//    @Suppress("UnusedPrivateMember")
+//    private fun fakeList(): List<Character> {
+//        return listOf(
+//            Character(
+//                "Spider Man",
+//                "https://lumiere-a.akamaihd.net/v1/images/marvelspidermanseries-emeagrid-m_4456fb99.jpeg"
+//            ),
+//            Character(
+//                "Spider Man",
+//                "https://lumiere-a.akamaihd.net/v1/images/marvelspidermanseries-emeagrid-m_4456fb99.jpeg"
+//            ),
+//            Character(
+//                "Spider Man",
+//                "https://lumiere-a.akamaihd.net/v1/images/marvelspidermanseries-emeagrid-m_4456fb99.jpeg"
+//            ),
+//            Character(
+//                "Spider Man",
+//                "https://lumiere-a.akamaihd.net/v1/images/marvelspidermanseries-emeagrid-m_4456fb99.jpeg"
+//            )
+//        )
+//    }
 
     private fun initCharactersAdapter() {
         with(binding.recyclerCharacter) {
