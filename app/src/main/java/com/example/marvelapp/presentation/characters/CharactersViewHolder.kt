b419ae71ee0c3976032c14e5ad7b1d993cdc524c
Claudio Soares <claudiolibanez@gmail.com>
2023-12-04 22:18:18 -0300

    Finaliza feature detalhe personagem

--- app/src/main/java/com/example/marvelapp/presentation/characters/CharactersViewHolder.kt
+++ app/src/main/java/com/example/marvelapp/presentation/characters/CharactersViewHolder.kt
@@ -1,15 +1,20 @@
 package com.example.marvelapp.presentation.characters
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.core.domain.model.Character
 import com.example.marvelapp.R
 import com.example.marvelapp.databinding.ItemCharacterBinding
+import com.example.marvelapp.framework.imageloader.ImageLoader
+import com.example.marvelapp.util.OnCharacterItemClick
 
 class CharactersViewHolder(
-    itemCharacterBinding: ItemCharacterBinding
+    itemCharacterBinding: ItemCharacterBinding,
+    private val imageLoader: ImageLoader,
+    private val onItemClick: OnCharacterItemClick
 ) : RecyclerView.ViewHolder(itemCharacterBinding.root) {
 
     private val textName = itemCharacterBinding.textName
@@ -17,18 +22,30 @@ class CharactersViewHolder(
 
     fun bind(character: Character) {
         textName.text = character.name
-        Glide.with(itemView)
-            .load(character.imageUrl)
-            .fallback(R.drawable.ic_img_loading_error)
-            .into(imageCharacter)
+        imageCharacter.transitionName = character.name
+
+        imageLoader.load(imageCharacter, character.imageUrl)
+
+//        Glide.with(itemView)
+//            .load(character.imageUrl)
+//            .fallback(R.drawable.ic_img_loading_error)
+//            .into(imageCharacter)
+
+        itemView.setOnClickListener {
+            onItemClick.invoke(character, imageCharacter)
+        }
     }
 
     companion object {
-        fun create(parent: ViewGroup): CharactersViewHolder {
+        fun create(
+            parent: ViewGroup,
+            imageLoader: ImageLoader,
+            onItemClick: (character: Character, view: View) -> Unit
+        ): CharactersViewHolder {
             val inflater = LayoutInflater.from(parent.context)
             val itemBinding = ItemCharacterBinding.inflate(inflater, parent, false)
 
-            return CharactersViewHolder(itemBinding)
+            return CharactersViewHolder(itemBinding, imageLoader, onItemClick)
         }
     }
 }
--- core/src/main/java/com/example/core/data/repository/CharactersRemoteDataSource.kt
+++ core/src/main/java/com/example/core/data/repository/CharactersRemoteDataSource.kt
@@ -1,6 +1,14 @@
 package com.example.core.data.repository
 
-interface CharactersRemoteDataSource<T> {
+import com.example.core.domain.model.CharacterPaging
+import com.example.core.domain.model.Comic
+import com.example.core.domain.model.Event
 
-    suspend fun fetchCharacters(queries: Map<String, String>): T
+interface CharactersRemoteDataSource {
+
+    suspend fun fetchCharacters(queries: Map<String, String>): CharacterPaging
+
+    suspend fun getComics(characterId: Int): List<Comic>
+
+    suspend fun  getEvents(characterId: Int): List<Event>
 }
